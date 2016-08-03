705e41961af230defe2b43ae37bc26d9f63e3f0d
Th FENEUIL <tfeneuil@free.fr>
2016-08-03 19:12:45 +0200

    Interface - Rapide ajout des kholles
    Rapide ajout des kholles en Orange

--- interface/khollotable.cpp
+++ interface/khollotable.cpp
@@ -74,6 +74,21 @@ void KholloTable::displayTable() {
                 areaCourses.append(rect);
             }
         }
+
+        QList<Kholle*>* listKholles = m_student->kholles();
+        for(int i=0; i<listKholles->count(); i++) { // For each group
+            Kholle* klle = listKholles->at(i);
+            Timeslot* slot = klle->timeslot();
+
+            // Display the course
+            int x = m_sizeImg[BeginDays]+m_sizeImg[BetweenDays]*m_monday.daysTo(slot->getDate());
+            int y = m_sizeImg[BeginHours]+m_sizeImg[BetweenHours]*(slot->getTime().msecsSinceStartOfDay() - QTime(8, 0).msecsSinceStartOfDay())/3600000;
+            int w = m_sizeImg[BetweenDays];
+            int h = m_sizeImg[BetweenHours]*(slot->getTime_end().msecsSinceStartOfDay() - slot->getTime().msecsSinceStartOfDay())/3600000;
+            QRect rect(x,y,w,h);
+            addRect(rect, QPen(Qt::black, 0), QBrush(QColor(255,201,14)));
+            areaCourses.append(rect);
+        }
     }
 
     if(m_kholleur) {
